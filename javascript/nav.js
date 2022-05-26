f978c4f6bbe151e712be7c01d28a68482c14b246
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2022-05-26 16:29:21 -0400

    Refactor main nav

--- javascript/nav.js
+++ javascript/nav.js
@@ -1,30 +1,54 @@
-function menuNav(e) {
-  var link = $(e.target);
-  var sectionName = link.attr('id').replace('menu-','');
+function MainNav() {
+  this.init = function () {
 
-  $('main').children().slideUp().appendTo($('.hidden'));
-  toggleMenuItem(link);
+    this.currentTab();
+    this.showDropdownMenu();
+    $('.menu a').click((e) => this.menuItemSelected(e));
 
-  var randomIndex = Math.round(Math.random());
-  var effect = ['drop', 'clip'][randomIndex];
-  $('.'+sectionName.replace('menu-','')).
-    appendTo($('main')).hide().toggle(effect);
+    loadTwitter();
+    // Icon bounce effect
+    iconHover();
+  };
+
+  this.menuItemSelected = function (e) {
+    var link = $(e.target);
+    var sectionName = link.attr('id').replace('menu-','');
+
+    $('main').children().slideUp().appendTo($('.hidden'));
+    this.toggleMenuItem(link);
+
+    var randomIndex = Math.round(Math.random());
+    var effect = ['drop', 'clip'][randomIndex];
+    $(`.${sectionName.replace('menu-','')}`).
+      appendTo($('main')).hide().toggle(effect);
+  };
+
+  this.toggleMenuItem = function (clickedItem) {
+    $('.current').removeClass('current');
+    clickedItem.addClass('current');
+  };
+
+  this.currentTab = function () {
+    if (location.hash === '') {
+      $('#menu-about').addClass('current');
+      $('.about').appendTo($('main')).hide().fadeIn('slow');
+    } else {
+      var sectionName = location.hash.replace('#','');
+      $('#menu-'+sectionName).addClass('current');
+      $('.'+sectionName).appendTo($('main')).hide().fadeIn('slow');
+    }
+  };
+
+  this.showDropdownMenu = function () {
+    console.warn('showDropdownMenu')
+    $('#menu-travel a').hover(function () {
+        console.warn('show menu')
+       // dropdown-menu
+    });
+  };
 }
 
-
-function toggleMenuItem(clickedItem) {
-  $('.current').removeClass('current');
-  clickedItem.addClass('current');
-}
-
-
-function currentTab() {
-  if (location.hash === '') {
-    $('#menu-about').addClass('current');
-    $('.about').appendTo($('main')).hide().fadeIn('slow');
-  } else {
-    var sectionName = location.hash.replace('#','');
-    $('#menu-'+sectionName).addClass('current');
-    $('.'+sectionName).appendTo($('main')).hide().fadeIn('slow');
-  }
-}
+$(document).ready(function() {
+  const nav = new MainNav();
+  nav.init();
+});
--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-  currentTab();
-  loadTwitter();
-  $('.menu a').click(menuNav);
-  iconHover();
-
-});
