0f6c747aca28d91b978dd5b837cbfacf0299ea54
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2022-05-27 14:15:22 -0400

    Style current page

--- travel/script/nav.js
+++ travel/script/nav.js
@@ -4,28 +4,35 @@ function Navigation(numPages) {
     this.init = function() {
         this.pages = [];
 
-        for (var i = 0; i < this.numPages; i++) {
-            var number = i + 1;
-            this.pages.push(number);
+        // Generate page numbers
+        for (let i = 0; i < this.numPages; i++) {
+            const pageNum = i + 1;
+            this.pages.push(pageNum);
 
-            var pageLabel = $('<a>').html(number);
-            pageLabel.attr('href', `#${number}`);
+            var pageLabel = $('<a>').html(pageNum);
+            pageLabel.attr('href', `#${pageNum}`);
+            pageLabel.addClass(`page-${pageNum}`);
             pageLabel.click(this.changePage);
             $('.navigation').append(pageLabel);
         }
     };
 
     this.currentPage = function() {
-        var pageNum = parseInt(window.location.hash.replace('#', ''));
+        let pageNum = parseInt(window.location.hash.replace('#', ''));
         if (this.pages.indexOf(pageNum) === -1) {
             pageNum = 1;
         }
+
+        $('.navigation .active').removeClass('active');
+        $(`.page-${pageNum}`).addClass('active');
         return pageNum;
     };
 
     this.changePage = function(event) {
-        var pageNum = parseInt(event.target.innerHTML);
+        $('.navigation .active').removeClass('active');
+        $(event.target).addClass('active');
 
+        const pageNum = parseInt(event.target.innerHTML);
         index.loadJson((data) => {
             new Gallery(data, pageNum);
         });
